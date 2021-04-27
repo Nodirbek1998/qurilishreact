a40dde26db6a131fe40a72881dd36f8a8a4c4505
user <nodirbekmamadaliyev1998@gmail.com>
2021-04-27 15:28:55 +0500

    svsdvd sd

--- architecture/src/services/api/index.js
+++ architecture/src/services/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://architecture-manager-web.herokuapp.com/",
   headers: {
     Accept: "*/*",
     "Content-Type": "application/json",
