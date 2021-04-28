80aa0126c984534c078445788474ce9d1f63ccc8
Nodirbek1998 <74307126+Nodirbek1998@users.noreply.github.com>
2021-04-28 16:10:55 +0500

    Update index.js

--- architecture/src/services/api/index.js
+++ architecture/src/services/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://architecture-frontend.herokuapp.com//",
+  baseURL: "https://architecture-frontend.herokuapp.com/",
   headers: {
     Accept: "*/*",
     "Content-Type": "application/json",
